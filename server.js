1ad3a9ea3ef0a80ff44dfab1ca3b86d469407821
nikkuts <nickkuts888@gmail.com>
2023-07-31 19:14:22 +0300

    Added the ability to download the user's avatar and test the login function

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.set('strictQuery', true);
 mongoose.Promise = global.Promise;
 
-mongoose.connect(DB_HOST)
+const mongoUri = process.env.NODE_ENV === 'test' ? 'mongodb://localhost/testdb' : DB_HOST;
+
+mongoose.connect(mongoUri)
   .then(() => {
     console.log('Database connection successful');
     app.listen(PORT);
